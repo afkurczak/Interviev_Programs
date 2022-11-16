07af5a3e0d2a7889616bd7ffd6a49369ce0902e8
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-11-16 21:40:40 +0100

    Add StringUtils and test Class

---
+++ src/StringUtilsTest.java
@@ -0,0 +1,7 @@
+public class StringUtilsTest {
+    public StringBuilder reverseTxtTest(String inputTxtTest) {
+        StringBuilder outputTxtTest = new StringBuilder(inputTxtTest);
+        outputTxtTest.reverse();
+        return outputTxtTest;
+    }
+}
