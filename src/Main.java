46972e67d0815fd6bd4e6701f226aab19c53946f
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-11-16 21:44:10 +0100

    Add StringUtils and test Class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Reverse reverse = new Reverse();
-        System.out.println(reverse.reverseTxt("ABCDEFG"));
+        String orgTxt = new String("ABCDEFG");
+
+        StringUtils reverse = new StringUtils();
+        StringUtilsTest reverseTest = new StringUtilsTest();
+
+        String compTxt = reverse.reverseTxt(orgTxt);
+        StringBuilder compTxtTest = reverseTest.reverseTxtTest(orgTxt);
+
+        System.out.println(compTxt);
+        System.out.println(compTxtTest);
     }
 }
