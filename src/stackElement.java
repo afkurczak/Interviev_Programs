8df637a28dac6e56fd5ff1b68fdc97c7b20e1a22
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-11-25 03:38:38 +0100

    Add stackElement

--- src/stackElement.java
+++ src/stackElement.java
@@ -1,4 +1,18 @@
+import java.util.*;
+
+
 public class stackElement {
-    int prevStackElement;
-    int stackObject;
+    private List stackElementPrev;
+    // public stackElement(){stackElementPrev = new List() {
+
+    //Object[] stackElementPrev;
+    //Object[] stackData;
+
+    public static void push() {
+        System.out.println("Podaj co wprowadzić na stos: ");
+        Scanner scanner = new Scanner(System.in);
+        Object stackData = scanner.next();
+
+    }
 }
+
