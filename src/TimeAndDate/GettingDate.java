9dfaa8e873464ab4dc69ef963e356c7083bb2947
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-12-04 12:37:23 +0100

    Calendar class test

---
+++ src/TimeAndDate/GettingDate.java
@@ -0,0 +1,18 @@
+package TimeAndDate;
+
+import java.util.Scanner;
+
+public class GettingDate {
+    public static void gettingDate() {
+        System.out.println("Podaj datę(YYYY.MM.DD):");
+        Scanner scanner = new Scanner(System.in);
+        int userDate = scanner.nextInt();
+
+
+    }
+
+    public static void GettingTime(){
+        System.out.println("Podaj godzinę:");
+
+    }
+}
--- src/TimeAndDate/MainDateAndTime.java
+++ src/TimeAndDate/MainDateAndTime.java
@@ -1,15 +1,21 @@
 package TimeAndDate;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MainDateAndTime {
     public static void WriteDateAndTime() {
-        Instant instant = Instant.now();
-        System.out.println(ZonedDateTime.now(ZoneId.of( "UTC+1" )).toString());
+        GettingDate.gettingDate();
 
-        System.out.println(instant);
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        //Instant instant = Instant.now();
+        //System.out.println(ZonedDateTime.now(ZoneId.of( "UTC+1" )).toString());
+        //System.out.println(ZonedDateTime.now(SimpleDateFormat("YYYY.MM.DD-HH.MM.SS")));
+        //System.out.println(instant);
     }
 }
