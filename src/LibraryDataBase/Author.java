5b19ddbb0d2c7ec24e058b100146d17caa325aed
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-12-14 19:31:26 +0100

    Create Author Class

--- src/LibraryDataBase/Author.java
+++ src/LibraryDataBase/Author.java
@@ -3,4 +3,19 @@ package LibraryDataBase;
 public class Author {
     String firstName;
     String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
 }
