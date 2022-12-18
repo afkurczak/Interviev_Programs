7c045e1bbbf88fb45d38b1f6b82a16d88868d630
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-12-18 23:05:12 +0100

    Test use method add

--- src/LibraryDataBase/MainLibraryDataBase.java
+++ src/LibraryDataBase/MainLibraryDataBase.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class MainLibraryDataBase implements Operation{
 
     @Override
-    public Long add(Book book ) {
+    public Long add(Book book) {
         Long bookId = 1L;
         return bookId;
     }
@@ -18,8 +18,13 @@ public class MainLibraryDataBase implements Operation{
     }
     public static void Main(){
         List<Book> bookDataBase = new ArrayList<>();
+        bookDataBase.add(new Book(0L,"dupa", "1234", LocalDate.now(), new Author("Dupa", "Dupa")));
         bookDataBase.add(new Book(1L,"dupa", "1234", LocalDate.now(), new Author("Dupa", "Dupa")));
+        Book book1 = new Book(3L, "e", "3", LocalDate.now(), new Author("a","s"));
+
+        //Long bookTest = MainLibraryDataBase.add(book1);
         System.out.println(bookDataBase.get(0));
+        System.out.println(bookDataBase.get(1));
 
     }
 }
--- src/LibraryDataBase/Operation.java
+++ src/LibraryDataBase/Operation.java
@@ -3,7 +3,7 @@ package LibraryDataBase;
 import java.time.LocalDate;
 
 public interface Operation  {
-    Long add(Book book);
+    Long  add(Book book);
     Book getById (Long id);
 }
 
