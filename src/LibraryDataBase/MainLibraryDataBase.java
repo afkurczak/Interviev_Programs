4fedc0330cf9a336165f774358b0f07cf6d755d0
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-12-19 03:30:32 +0100

    Add null check

--- src/LibraryDataBase/MainLibraryDataBase.java
+++ src/LibraryDataBase/MainLibraryDataBase.java
@@ -10,26 +10,45 @@ public class MainLibraryDataBase {
         InMemoryDataBase db = new InMemoryDataBase();
 
         Book book = new Book("Krzyżacy", "1111", LocalDate.of(1900,7,1), new Author("Henryk","Sienkiewicz"));
-        db.add(book);
+        if (book.getTitle() == null || book.getIsbn() == null || book.getReleaseTime() == null || book.getAuthor().firstName == null || book.getAuthor().lastName == null){}
+        else {
+            db.add(book);
+        }
 
         book = new Book ("Władca Pierścieni", "2222", LocalDate.of(1954,7,29), new Author("J.R.R.","Tolkien"));
-        db.add(book);
+        if (book.getTitle() == null || book.getIsbn() == null || book.getReleaseTime() == null || book.getAuthor().firstName == null || book.getAuthor().lastName == null){}
+        else {
+            db.add(book);
+        }
 
         book = new Book ("Wiedźmin", "83-7054-061-9", LocalDate.of(1993,1,1), new Author("Andrzej","Sapkowski"));
-        db.add(book);
+        if (book.getTitle() == null || book.getIsbn() == null || book.getReleaseTime() == null || book.getAuthor().firstName == null || book.getAuthor().lastName == null){}
+        else {
+            db.add(book);
+        }
 
-        book = new Book ("Wiedźmin", "83-7054-0619", LocalDate.of(1993,1,1), new Author("Andrzej","Sapkowski"));
-        db.add(book);
+        book = new Book ("Wiedźmin", "83-7054-061-9", LocalDate.of(1993,1,1), new Author("Andrzej","Sapkowski"));
+        if (book.getTitle() == null || book.getIsbn() == null || book.getReleaseTime() == null || book.getAuthor().firstName == null || book.getAuthor().lastName == null){}
+        else {
+            db.add(book);
+        }
 
-        // book = new Book (null, null, null, null);
-        //db.add(book);
+        book = new Book (null, null, null, null);
+        if (book.getTitle() == null || book.getIsbn() == null || book.getReleaseTime() == null || book.getAuthor().firstName == null || book.getAuthor().lastName == null){}
+        else {
+            db.add(book);
+        }
 
-        book = new Book ("Gra o tron", "4444", LocalDate.now(), new Author("George","Martin"));
-        db.add(book);
+        book = new Book ("Gra o tron", "4444", LocalDate.of(1980,1,1), new Author("George","Martin"));
+        if (book.getTitle() == null || book.getIsbn() == null || book.getReleaseTime() == null || book.getAuthor().firstName == null || book.getAuthor().lastName == null){}
+        else {
+            db.add(book);
+        }
 
 
-        //Book book1 = new InMemoryDataBase().getById(1L);
-        //System.out.println(book1);
+        Book book1 = new InMemoryDataBase().getById(4L);
+        System.out.println(book1);
+        System.out.println();
 
         InMemoryDataBase.allDataWrite();
 
