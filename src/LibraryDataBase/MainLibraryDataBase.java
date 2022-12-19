83dfae55e838755c07770b0a9658e6e3a5601669
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-12-19 01:00:39 +0100

    Working Add and getById Method ver. 0,3

--- src/LibraryDataBase/MainLibraryDataBase.java
+++ src/LibraryDataBase/MainLibraryDataBase.java
@@ -7,17 +7,19 @@ public class MainLibraryDataBase {
 
 
     public static void MainDataBase(){
+        InMemoryDataBase db = new InMemoryDataBase();
+
         Book book = new Book("Krzyżacy", "1111", LocalDate.now(), new Author("Henryk","Sienkiewicz"));
-        new InMemoryDataBase().add(book);
+        db.add(book);
 
-        book = new Book("Władca Pierścieni", "2222", LocalDate.now(), new Author("J.R.R.","Tolkien"));
-        new InMemoryDataBase().add(book);
+        book = new Book ("Władca Pierścieni", "2222", LocalDate.now(), new Author("J.R.R.","Tolkien"));
+        db.add(book);
 
-        book = new Book("Wiedźmin", "3333", LocalDate.now(), new Author("Andrzej","Sapkowski"));
-        new InMemoryDataBase().add(book);
+        book = new Book ("Wiedźmin", "3333", LocalDate.now(), new Author("Andrzej","Sapkowski"));
+        db.add(book);
 
 
-        Book book1 = new InMemoryDataBase().getById(3L);
+        Book book1 = new InMemoryDataBase().getById(2L);
         System.out.println(book1);
 
     }
