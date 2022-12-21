fa09e4e4ebcff88f2891905b8f495cf850d77493
Adrian Fleszar <adrian.fleszar@gmail.com>
2022-12-21 01:13:09 +0100

    IllegalArgumentException for null book record

--- src/LibraryDataBase/MainLibraryDataBase.java
+++ src/LibraryDataBase/MainLibraryDataBase.java
@@ -13,20 +13,21 @@ public class MainLibraryDataBase {
             Book book = new Book("Krzyżacy", "1111", LocalDate.of(1900, 7, 1), new Author("Henryk", "Sienkiewicz"));
             db.add(book);
 
-        book = new Book("Władca Pierścieni", "2222", LocalDate.of(1954, 7, 29), new Author("J.R.R.", "Tolkien"));
-        db.add(book);
+            book = new Book("Władca Pierścieni", "2222", LocalDate.of(1954, 7, 29), new Author("J.R.R.", "Tolkien"));
+            db.add(book);
 
-        book = new Book("Wiedźmin", "83-7054-061-9", LocalDate.of(1993, 1, 1), new Author("Andrzej", "Sapkowski"));
-        db.add(book);
+            book = new Book("Wiedźmin", "83-7054-061-9", LocalDate.of(1993, 1, 1), new Author("Andrzej", "Sapkowski"));
+            db.add(book);
 
-        book = new Book("Wiedźmin", "83-7054-061-9", LocalDate.of(1993, 1, 1), new Author("Andrzej", "Sapkowski"));
-        db.add(book);
+            book = new Book("Wiedźmin", "83-7054-061-9", LocalDate.of(1993, 1, 1), new Author("Andrzej", "Sapkowski"));
+            db.add(book);
 
-        book = new Book(null, null, null, null);
-        db.add(book);
+            book = new Book(null, null, null, null);
+            db.add(book);
 
-        book = new Book("Gra o tron", "4444", LocalDate.of(1980, 1, 1), new Author("George", "Martin"));
-        db.add(book);
+            book = new Book("Gra o tron", "4444", LocalDate.of(1980, 1, 1), new Author("George", "Martin"));
+            db.add(book);
+
     }catch(Exception exception)
     {
         System.out.println("Cannot add book (notnull expected)");
