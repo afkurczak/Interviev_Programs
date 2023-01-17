c3d763556866881b9b5452f752e681035eebfba2
Adrian Fleszar <adrian.fleszar@gmail.com>
2023-01-17 23:47:10 +0100

    In Class Windows Class: add working buttons and background

--- src/LibraryDataBase/Frame/MainFrame.java
+++ src/LibraryDataBase/Frame/MainFrame.java
@@ -1,9 +1,17 @@
 package LibraryDataBase.Frame;
 
 import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
 
 public class MainFrame {
+
     public static void MainFrame() {
+        StartWindow frame = new StartWindow();
+        frame.setVisible(true);
+    }
+
+    public static void MainFrame1() {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
--- src/LibraryDataBase/Frame/StartWindow.java
+++ src/LibraryDataBase/Frame/StartWindow.java
@@ -7,34 +7,42 @@ import java.awt.event.ActionListener;
 
 public class StartWindow extends JFrame {
     public StartWindow() {
-        super("Library Data Base");
+        setTitle("Library Data Base");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
         setLocation(100, 100);
-        JPanel imagePanel = new BackgroundImage();
-        add(imagePanel);
+        setLayout(null);
 
-        //setLayout(new GridLayout(2, 1, 10, 10));
-        setLayout(new FlowLayout());
-        setPreferredSize(new Dimension(200,100));
-        add(new JButton("Add new book"));
-        add(new JButton("Get by ID"));
-        add(new JButton("Find by ISBN"));
-        add(new JButton("Find by Author"));
-        add(new JButton("Find by Title"));
-        add(new JButton("Delete by ID"));
-        add(new JButton("Update by ID"));
+        ImageIcon background = new ImageIcon("background.jpg");
+        JLabel label = new JLabel(background);
+        label.setBounds(0, 0, 800, 600);
+        getContentPane().add(label);
 
-        setVisible(true);
+        JButton button1 = new JButton("Add new Book");
+        JButton button2 = new JButton("Get by ID");
+        JButton button3 = new JButton("Find by ISBN");
+        JButton button4 = new JButton("Find by Author");
+        JButton button5 = new JButton("Find by Title");
+        JButton button6 = new JButton("Delete by ID");
+        JButton button7 = new JButton("Update by ID");
+
+        button1.setBounds(50, 50, 200, 30);
+        button2.setBounds(50, 90, 200, 30);
+        button3.setBounds(50, 130, 200, 30);
+        button4.setBounds(50, 170, 200, 30);
+        button5.setBounds(50, 210, 200, 30);
+        button6.setBounds(50, 250, 200, 30);
+        button7.setBounds(50, 290, 100, 20);
+
+        getContentPane().add(button1);
+        getContentPane().add(button2);
+        getContentPane().add(button3);
+        getContentPane().add(button4);
+        getContentPane().add(button5);
+        getContentPane().add(button6);
+        getContentPane().add(button7);
+
+        //((JPanel) getContentPane()).setOpaque(false);
+        getContentPane().setBackground(new Color(255, 255, 255, 0));
     }
-
-    class ButtonAddNewBook extends JButton implements ActionListener{
-        ButtonAddNewBook(){
-            addActionListener(this);
-        }
-        @Override
-        public void actionPerformed(ActionEvent e){
-           // StartWindow.setBackground(Color.GREEN);
-        }
     }
-}
